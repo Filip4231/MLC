92e71af8beee8cad795f9ff6258ba69ab0a53716
Filip Binda <filipbinda6@gmail.com>
2025-11-06 19:20:00 +0100

    Date added to messages

--- app.py
+++ app.py
@@ -4,8 +4,17 @@ from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
+from datetime import datetime
 
 from helpers import login_required
+
+def time():
+    now = str(datetime.now())
+    now = now[0:19]
+    hour = now[-8:]
+    date=now[0:10]
+    return hour + " " + date
+
 
 app = Flask(__name__)
 
@@ -33,16 +42,16 @@ def login():
         return render_template("login.html")
     else:
         if not request.form.get("username"):
-            return render_template("login.html", massage="Please provide username")
+            return render_template("login.html", message="Please provide username")
         if not request.form.get("password"):
-            return render_template("login.html", massage="Please provide password")
+            return render_template("login.html", message="Please provide password")
 
         user = db.execute("SELECT * FROM users WHERE username=?", request.form.get("username"))
         if len(user) == 0:
-            return render_template("login.html", massage="Wrong username and/or password")
+            return render_template("login.html", message="Wrong username and/or password")
 
         if not check_password_hash(user[0]["password"], request.form.get("password")):
-            return render_template("login.html", massage="Wrong username and/or password")
+            return render_template("login.html", message="Wrong username and/or password")
         
         session["user_id"] = user[0]["id"]
         return redirect("/")
@@ -53,16 +62,17 @@ def signup():
         return render_template("signup.html")
     else:
         if not request.form.get("username"):
-            return render_template("signup.html", massage="Please provide username")
+            return render_template("signup.html", message="Please provide username")
         if not request.form.get("password") or not request.form.get("confirmation"):
-            return render_template("signup.html", massage="Please provide password and confirm password")
+            return render_template("signup.html", message="Please provide password and confirm password")
+
         if request.form.get("password") != request.form.get("confirmation"):
-            return render_template("signup.html", massage="Passwords are not identical")
+            return render_template("signup.html", message="Passwords are not identical")
 
         users = db.execute("SELECT * FROM users WHERE username LIKE ?", request.form.get("username"))
         print(users)
         if len(users) != 0:
-            return render_template("signup.html", massage="Username is taken")
+            return render_template("signup.html", message="Username is taken")
 
         print(request.form.get("username") + " " + generate_password_hash(request.form.get("password")))
         db.execute("INSERT INTO users(username, password) VALUES(?,?)", request.form.get("username"), generate_password_hash(request.form.get("password")))
@@ -87,8 +97,8 @@ def logout():
 def chats():
     #if request.args.get("friend"):
     #    print(request.args.get("friend"))
-    #    massages = db.execute("SELECT * FROM massages WHERE sender=? AND receiver=?", session["user_id"], request.args.get("friend"))
-    #    return redirect("chat.html", friend1 = session["user_id"], friend2=request.args.get("friend"), massages=massages)
+    #    messages = db.execute("SELECT * FROM messages WHERE sender=? AND receiver=?", session["user_id"], request.args.get("friend"))
+    #    return redirect("chat.html", friend1 = session["user_id"], friend2=request.args.get("friend"), messages=messages)
     #else:
         friends = db.execute("SELECT * FROM friends WHERE friend1=? or friend2=?", session["user_id"], session["user_id"])
         for row in friends:
@@ -108,31 +118,18 @@ def chat():
     if not request.args.get("friend"):
         return redirect("/chats")
     else:
-        massages = db.execute("SELECT * FROM massages WHERE (sender=? AND receiver=?) OR (sender=? AND receiver=?)", session["user_id"], request.args.get("friend"), request.args.get("friend"), session["user_id"])
-<<<<<<< HEAD
-        print(massages)
-        print(request.args.get("friend"))
-        #print(massages[len(massages)-1]["sender"])
-        if request.form.get("massage") and (len(massages)==0 or massages[len(massages)-1]["massage"] != request.form.get("massage" or massages[len(massages)-1]["sender"] != request.args.grt("friend"))) :
-            print(request.form.get("massage"))
-            db.execute("INSERT INTO massages(sender,receiver,massage) VALUES(?,?,?)", session["user_id"], request.args.get("friend"), request.form.get("massage"))
+        messages = db.execute("SELECT * FROM messages WHERE (sender=? AND receiver=?) OR (sender=? AND receiver=?)", session["user_id"], request.args.get("friend"), request.args.get("friend"), session["user_id"])
+        if request.form.get("message") and (len(messages)==0 or messages[len(messages)-1]["message"] != request.form.get("message")) :
+            print(request.form.get("message"))
+            db.execute("INSERT INTO messages(sender,receiver,message,date) VALUES(?,?,?,?)", session["user_id"], request.args.get("friend"), request.form.get("message"), time())
 
-        massages = db.execute("SELECT * FROM massages WHERE (sender=? AND receiver=?) OR (sender=? AND receiver=?)", session["user_id"], request.args.get("friend"), request.args.get("friend"), session["user_id"])
-        print(massages)
+        messages = db.execute("SELECT * FROM messages WHERE (sender=? AND receiver=?) OR (sender=? AND receiver=?)", session["user_id"], request.args.get("friend"), request.args.get("friend"), session["user_id"])
+        print(messages)
         print(request.args.get("friend"))
-=======
-        if request.form.get("massage") and (len(massages)==0 or massages[len(massages)-1]["massage"] != request.form.get("massage")) :
-            print(request.form.get("massage"))
-            db.execute("INSERT INTO massages(sender,receiver,massage) VALUES(?,?,?)", session["user_id"], request.args.get("friend"), request.form.get("massage"))
-
-        massages = db.execute("SELECT * FROM massages WHERE (sender=? AND receiver=?) OR (sender=? AND receiver=?)", session["user_id"], request.args.get("friend"), request.args.get("friend"), session["user_id"])
-        print(massages)
-        print(request.args.get("friend"))
->>>>>>> a103fa5dbd6869832d8a153456aac08dede81eab
         print(session["user_id"])
         friendname = db.execute("SELECT * FROM users WHERE id=?", request.args.get("friend"))[0]["username"]
         username = db.execute("SELECT * FROM users WHERE id=?", session["user_id"])[0]["username"]
-        return render_template("chat.html", friend1_username=username, friend2_username=friendname, friend1=session["user_id"], friend2=request.args.get("friend"), massages=massages)
+        return render_template("chat.html", friend1_username=username, friend2_username=friendname, friend1=session["user_id"], friend2=request.args.get("friend"), messages=messages)
 
 
 
@@ -144,7 +141,7 @@ def addfriend():
         return render_template("addfriend.html")
     else:
         if not request.form.get("username"):
-            return render_template("addfriend.html", massage="Provide friend's username")
+            return render_template("addfriend.html", message="Provide friend's username")
         friends = db.execute("SELECT * FROM users WHERE username LIKE ? AND id!=?", "%" + request.form.get("username") + "%", session["user_id"])
         print(friends)
         return render_template("addfriend.html", friends=friends)
