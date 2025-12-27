738fc316ded441e39347e4304669ed03caf3a3f8
Filip Binda <filipbinda6@gmail.com>
2025-12-27 14:42:17 +0100

    Fixed formatting in requirements.txt

--- app.py
+++ app.py
@@ -18,6 +18,9 @@ def time():
 
 app = Flask(__name__)
 
+if __name__ == '__main__':
+    app.run()
+
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
